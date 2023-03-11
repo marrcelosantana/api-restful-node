564c63cdc3332ab6ec03200b32630a27faed2f5d
marrcelosantana <marcelosantana@alu.ufc.br>
2023-03-11 13:41:45 -0300

    [API-Restful] - Add tests

---
+++ test/transactions.spec.ts
@@ -0,0 +1,50 @@
+import { test, expect, beforeAll, afterAll, describe } from "vitest";
+import request from "supertest";
+import { app } from "../src/app";
+
+describe("Transactions routes", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  test("user can create a new transaction", async () => {
+    const response = await request(app.server).post("/transactions").send({
+      title: "New transaction",
+      amount: 5000,
+      type: "credit",
+    });
+
+    expect(response.statusCode).toEqual(201);
+
+    console.log(response.get("Set-Cookie"));
+  });
+
+  test("user can list all transactions", async () => {
+    const createTransactionResponse = await request(app.server)
+      .post("/transactions")
+      .send({
+        title: "New transaction",
+        amount: 5000,
+        type: "credit",
+      });
+
+    const cookies = createTransactionResponse.get("Set-Cookie");
+
+    const listTransactionsResponse = await request(app.server)
+      .get("/transactions")
+      .set("Cookie", cookies);
+
+    expect(listTransactionsResponse.statusCode).toEqual(200);
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: "New transaction",
+        amount: 5000,
+      }),
+    ]);
+  });
+});
