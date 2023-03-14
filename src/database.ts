a7ccc62ead2d47416179cdc3149dd93a98d4507c
marrcelosantana <marcelosantana@alu.ufc.br>
2023-03-14 13:33:32 -0300

    [API-Restful] - fix bug on variable 3

--- src/database.ts
+++ src/database.ts
@@ -10,7 +10,7 @@ export const config: Knex.Config = {
       ? {
           filename: env.DATABASE_URL,
         }
-      : env.DATABASE_CLIENT,
+      : env.DATABASE_URL,
   useNullAsDefault: true,
   migrations: {
     extension: "ts",
